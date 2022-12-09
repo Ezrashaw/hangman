b2024aba421317b21fa241b7296c205c5ebfce32
Ezra Shaw <ezrasure@outlook.com>
2022-12-10 10:33:30 +1300

    Initial commit

---
+++ src/hangman.rs
@@ -0,0 +1,98 @@
+use libc_print::*;
+
+pub struct Hangman<'a> {
+    word: &'a str,
+    letters_used: u32,
+    guesses_remaining: u32,
+}
+
+impl<'a> Hangman<'a> {
+    pub fn new(word: &'a str, guesses_remaining: u32) -> Self {
+        Self {
+            word,
+            letters_used: 0,
+            guesses_remaining,
+        }
+    }
+
+    pub fn state(&self) -> HangmanState {
+        let mut won = true;
+        for l in self.word.as_bytes() {
+            if !self.is_letter_used(*l) {
+                won = false;
+                break;
+            }
+        }
+
+        if won {
+            HangmanState::Won
+        } else if self.guesses_remaining == 0 {
+            HangmanState::Lost
+        } else {
+            HangmanState::Playing
+        }
+    }
+
+    pub fn guess(&mut self) {
+        self.guess_prompt();
+        let letter = self.input_guess();
+        if !self.word.contains(letter as char) {
+            self.guesses_remaining -= 1;
+        }
+        self.set_letter_used(letter);
+    }
+
+    fn input_guess(&self) -> u8 {
+        loop {
+            // SAFETY: `libc::getchar` has no invariants to uphold.
+            let c = unsafe { libc::getchar() as u8 };
+
+            if c.is_ascii_lowercase() && !self.is_letter_used(c) {
+                break c;
+            }
+        }
+    }
+
+    fn guess_prompt(&self) {
+        libc_print!(
+            "\x1Bc--- HANGMAN ---\nYou have {} guesses remaining.\nLetters used: ",
+            self.guesses_remaining
+        );
+
+        for l in 'a'..='z' {
+            if self.is_letter_used(l as u8) {
+                libc_print!("{} ", l);
+            }
+        }
+        libc_println!();
+
+        for l in self.word.chars() {
+            if self.is_letter_used(l as u8) {
+                libc_print!("{}", l);
+            } else {
+                libc_print!("_");
+            }
+        }
+        libc_print!("\n\nPlease guess a letter...");
+    }
+
+    fn is_letter_used(&self, letter: u8) -> bool {
+        let bit_index = letter - b'a';
+        let used_shifted = self.letters_used >> bit_index;
+
+        used_shifted & 0b1 == 0b1
+    }
+
+    fn set_letter_used(&mut self, letter: u8) {
+        let bit_index = letter - b'a';
+        let used_shifted = 0b1u32 << bit_index;
+
+        self.letters_used |= used_shifted
+    }
+}
+
+pub enum HangmanState {
+    Playing,
+    Won,
+    Lost,
+}
---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+#![no_std]
+
+use hangman::Hangman;
+use libc_print::libc_println;
+use rand::Rng;
+use termios::Termios;
+
+pub mod hangman;
+
+pub fn main() {
+    let mut termios = Termios::from_fd(libc::STDIN_FILENO).unwrap();
+    termios.c_lflag &= !termios::ECHO;
+    termios.c_lflag &= !termios::ICANON;
+    termios::tcsetattr(libc::STDIN_FILENO, termios::TCSANOW, &termios).unwrap();
+
+    let mut rng = rand::thread_rng();
+
+    let word = WORDS
+        .split('\n')
+        .skip(rng.gen_range(0..count_lines(WORDS)))
+        .next()
+        .unwrap();
+
+    let mut hangman = Hangman::new(word, 12);
+
+    loop {
+        hangman.guess();
+
+        match hangman.state() {
+            hangman::HangmanState::Playing => (),
+            hangman::HangmanState::Won => {
+                libc_println!("\nYou won!");
+                break;
+            }
+            hangman::HangmanState::Lost => {
+                libc_println!("\nGame over! The word was {}.", word);
+                break;
+            }
+        }
+    }
+}
+
+const WORDS: &str = include_str!("words.txt");
+
+fn count_lines(s: &str) -> usize {
+    s.as_bytes().iter().filter(|&&c| c == b'\n').count() + 1
+}
